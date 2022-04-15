a0f99544cb09c61062b4dc3c6ff89f7b5c1fd0fc
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-04-15 13:18:59 +0200

    HttpServer sends hard coded response

--- src/HttpServer.java
+++ src/HttpServer.java
@@ -7,6 +7,17 @@ public class HttpServer {
         ServerSocket serverSocket = new ServerSocket(8080);
         Socket clientSocket = serverSocket.accept();
         System.out.println("Client connected");
+
+        String body = "Hello world";
+        clientSocket.getOutputStream().write((
+                "HTTP/1.1 200 OK\r\n" +
+                "Connection: close\r\n" +
+                "Content-type: text/html\r\n" +
+                "Content-length: " + body.length() + "\r\n" +
+                "\r\n" +
+                body
+                ).getBytes());
+
         int c;
         while ((c = clientSocket.getInputStream().read()) != -1) {
             System.out.print((char)c);
