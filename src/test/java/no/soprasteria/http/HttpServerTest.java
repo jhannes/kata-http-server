0d39453b89139c70b30f0a2ad3c8a81e150d2727
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 22:35:03 +0100

    Http server should include request path in 404 message (fails with unique url)

--- src/test/java/no/soprasteria/http/HttpServerTest.java
+++ src/test/java/no/soprasteria/http/HttpServerTest.java
@@ -6,20 +6,23 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HttpServerTest {
+    private static final Random random = new Random();
 
     @Test
     void shouldRespondWith404ToUnknownPages() throws IOException {
         var server = new HttpServer(0);
         server.startServer();
-        var connection = (HttpURLConnection) new URL(server.getURL(), "/unknown-url").openConnection();
+        var path = "/unknown-url-" + random.nextInt();
+        var connection = (HttpURLConnection) new URL(server.getURL(), path).openConnection();
         assertEquals(404, connection.getResponseCode());
 
         var buffer = new ByteArrayOutputStream();
         connection.getErrorStream().transferTo(buffer);
-        assertEquals("Unknown file /unknown-url", buffer.toString());
+        assertEquals("Unknown file " + path, buffer.toString());
     }
 }
