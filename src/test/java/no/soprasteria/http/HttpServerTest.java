d9aee2f7af7b02f01ecf5054da0f0f02a75a0043
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 22:40:45 +0100

    Refactor HttpServerTest for more tests

--- src/test/java/no/soprasteria/http/HttpServerTest.java
+++ src/test/java/no/soprasteria/http/HttpServerTest.java
@@ -1,9 +1,11 @@
 package no.soprasteria.http;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Random;
@@ -12,17 +14,29 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HttpServerTest {
     private static final Random random = new Random();
+    private HttpServer server;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        server = new HttpServer(0);
+        server.startServer();
+    }
 
     @Test
     void shouldRespondWith404ToUnknownPages() throws IOException {
-        var server = new HttpServer(0);
-        server.startServer();
         var path = "/unknown-url-" + random.nextInt();
-        var connection = (HttpURLConnection) new URL(server.getURL(), path).openConnection();
+        var connection = getOpenConnection(path);
         assertEquals(404, connection.getResponseCode());
+        assertEquals("Unknown file " + path, asString(connection.getErrorStream()));
+    }
 
+    private static String asString(InputStream stream) throws IOException {
         var buffer = new ByteArrayOutputStream();
-        connection.getErrorStream().transferTo(buffer);
-        assertEquals("Unknown file " + path, buffer.toString());
+        stream.transferTo(buffer);
+        return buffer.toString();
+    }
+
+    private HttpURLConnection getOpenConnection(String path) throws IOException {
+        return (HttpURLConnection) new URL(server.getURL(), path).openConnection();
     }
 }
