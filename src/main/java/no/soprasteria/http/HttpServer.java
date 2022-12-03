9e8fcd87229013cbeb9747fedaeb1c36059ee2c7
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 22:18:54 +0100

    HttpServer responds with chunked encoding

--- src/main/java/no/soprasteria/http/HttpServer.java
+++ src/main/java/no/soprasteria/http/HttpServer.java
@@ -13,7 +13,7 @@ public class HttpServer {
         var clientSocket = socket.accept();
 
         var body = "hallæ værden!";
-        int contentLength = body.getBytes(StandardCharsets.UTF_8).length;
+        var contentLength = body.getBytes(StandardCharsets.UTF_8).length;
         var response = "HTTP/1.1 200 OK\r\n" +
                        "Connection: close\r\n" +
                        "Content-Type: text/html; charset=utf-8\r\n" +
