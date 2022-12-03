70d6ca360318765267a79658fb0a62b7f00c1882
Johannes Brodwall <johannes@brodwall.com>
2022-12-03 22:08:28 +0100

    HttpServer responds with static content

--- src/main/java/no/soprasteria/http/HttpServer.java
+++ src/main/java/no/soprasteria/http/HttpServer.java
@@ -10,6 +10,16 @@ public class HttpServer {
         var socket = new ServerSocket(8080);
 
         var clientSocket = socket.accept();
+
+        var body = "hello world";
+        var response = "HTTP/1.1 200 OK\r\n" +
+                       "Connection: close\r\n" +
+                       "Content-Length: " + body.length() + "\r\n" +
+                       "\r\n" +
+                       body;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+
         int c;
         while ((c = clientSocket.getInputStream().read()) != -1) {
             System.out.print((char)c);
