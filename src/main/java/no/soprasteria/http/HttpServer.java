da33459ebde832dea90cf6f14bc63877c09c23d0
Johannes Brodwall <johannes@brodwall.com>
2022-12-05 22:22:35 +0100

    Dummy HttpServer

---
+++ src/main/java/no/soprasteria/http/HttpServer.java
@@ -0,0 +1,17 @@
+package no.soprasteria.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class HttpServer {
+
+    public static void main(String[] args) throws IOException {
+        @SuppressWarnings("resource") var serverSocket = new ServerSocket(8080);
+
+        var clientSocket = serverSocket.accept();
+        int c;
+        while ((c = clientSocket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
+    }
+}
