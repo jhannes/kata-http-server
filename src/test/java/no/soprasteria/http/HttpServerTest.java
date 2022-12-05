fc4f5a3b055c49c149a84e3b1cc7ce74f3d8d5c2
Johannes Brodwall <johannes@brodwall.com>
2022-12-05 22:40:05 +0100

    🔴 HttpServer should return request target with 404

--- src/test/java/no/soprasteria/http/HttpServerTest.java
+++ src/test/java/no/soprasteria/http/HttpServerTest.java
@@ -2,6 +2,7 @@ package no.soprasteria.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -16,5 +17,9 @@ class HttpServerTest {
         server.start();
         var connection = (HttpURLConnection) new URL(server.getURL(), "/no/such/file").openConnection();
         assertEquals(404, connection.getResponseCode());
+        var responseBuffer = new ByteArrayOutputStream();
+        connection.getErrorStream().transferTo(responseBuffer);
+        var responseBody = responseBuffer.toString();
+        assertEquals("Not found /no/such/file", responseBody);
     }
 }
