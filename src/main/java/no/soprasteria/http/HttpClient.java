6236e3d73a651aaaf0a5bc4cff8838161036bf91
Johannes Brodwall <johannes@brodwall.com>
2022-12-05 22:27:31 +0100

    Dummy HttpClient with SSL

--- src/main/java/no/soprasteria/http/HttpClient.java
+++ src/main/java/no/soprasteria/http/HttpClient.java
@@ -1,12 +1,14 @@
 package no.soprasteria.http;
 
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.IOException;
 import java.net.Socket;
 
 public class HttpClient {
 
     public static void main(String[] args) throws IOException {
-        var socket =  new Socket("www.rfc-editor.org", 80);
+        var socket =  SSLSocketFactory.getDefault().createSocket("www.rfc-editor.org", 443);
 
         socket.getOutputStream().write("""
                 GET / HTTP/1.1\r
