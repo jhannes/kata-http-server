0f034168a31c2f6685ba09d462996b71b9c93734
Johannes Brodwall <johannes@brodwall.com>
2022-12-05 22:27:06 +0100

    Dummy HttpClient

---
+++ src/main/java/no/soprasteria/http/HttpClient.java
@@ -0,0 +1,24 @@
+package no.soprasteria.http;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class HttpClient {
+
+    public static void main(String[] args) throws IOException {
+        var socket =  new Socket("www.rfc-editor.org", 80);
+
+        socket.getOutputStream().write("""
+                GET / HTTP/1.1\r
+                Host: www.rfc-editor.org\r
+                Connection: close\r
+                \r
+                """.getBytes());
+        int c;
+        while ((c = socket.getInputStream().read()) != -1) {
+            System.out.print((char)c);
+        }
+
+    }
+
+}
