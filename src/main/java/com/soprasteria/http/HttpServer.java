3af084a6e1b4b3dd49dde7f790007dc9e5cc1a82
Johannes Brodwall <johannes@brodwall.com>
2023-02-14 23:01:15 +0100

    🔵 Refactoring: Extract HttpServerClient class (step 2/5)

--- src/main/java/com/soprasteria/http/HttpServer.java
+++ src/main/java/com/soprasteria/http/HttpServer.java
@@ -3,9 +3,7 @@ package com.soprasteria.http;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class HttpServer {
@@ -23,48 +21,10 @@ public class HttpServer {
     private void runServer() {
         try {
             var clientSocket = serverSocket.accept();
-            handleClient(clientSocket, httpRoot);
+            HttpServerClient.handleClient(clientSocket, httpRoot);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private static void handleClient(Socket clientSocket, Path httpRoot1) throws IOException {
-        String requestLine = readLine(clientSocket);
-        var requestTarget = requestLine.split(" ")[1];
-
-        var requestFile = httpRoot1.resolve(requestTarget.substring(1));
-        if (Files.exists(requestFile)) {
-            var body = Files.readString(requestFile);
-            clientSocket.getOutputStream().write("""
-                HTTP/1.1 200 OK\r
-                Content-Length: %d\r
-                Connection: close\r
-                \r
-                %s""".formatted(body.length(), body).getBytes());
-
-        } else {
-            var body = "Unknown path " + requestTarget;
-            clientSocket.getOutputStream().write("""
-                HTTP/1.1 404 Not found\r
-                Content-Length: %d\r
-                Connection: close\r
-                Content-type: text/html\r
-                \r
-                %s""".formatted(body.length(), body).getBytes());
-        }
-    }
-
-    private static String readLine(Socket clientSocket) throws IOException {
-        var line = new StringBuilder();
-        int c;
-        while((c = clientSocket.getInputStream().read()) != -1) {
-            if (c == '\n') {
-                break;
-            }
-            line.append((char)c);
-        }
-        return line.toString();
     }
 
     public URL getURL() throws MalformedURLException {
---
+++ src/main/java/com/soprasteria/http/HttpServerClient.java
@@ -0,0 +1,52 @@
+package com.soprasteria.http;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class HttpServerClient {
+    private final Socket clientSocket;
+
+    public HttpServerClient(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    static void handleClient(Socket clientSocket, Path httpRoot1) throws IOException {
+        String requestLine = readLine(clientSocket);
+        var requestTarget = requestLine.split(" ")[1];
+
+        var requestFile = httpRoot1.resolve(requestTarget.substring(1));
+        if (Files.exists(requestFile)) {
+            var body = Files.readString(requestFile);
+            clientSocket.getOutputStream().write("""
+                HTTP/1.1 200 OK\r
+                Content-Length: %d\r
+                Connection: close\r
+                \r
+                %s""".formatted(body.length(), body).getBytes());
+
+        } else {
+            var body = "Unknown path " + requestTarget;
+            clientSocket.getOutputStream().write("""
+                HTTP/1.1 404 Not found\r
+                Content-Length: %d\r
+                Connection: close\r
+                Content-type: text/html\r
+                \r
+                %s""".formatted(body.length(), body).getBytes());
+        }
+    }
+
+    private static String readLine(Socket clientSocket) throws IOException {
+        var line = new StringBuilder();
+        int c;
+        while((c = clientSocket.getInputStream().read()) != -1) {
+            if (c == '\n') {
+                break;
+            }
+            line.append((char)c);
+        }
+        return line.toString();
+    }
+}
