15881012a8054d81fe0aa2bc6b049a6d90200c53
Johannes Brodwall <johannes@brodwall.com>
2023-02-14 22:26:14 +0100

    Very naive HttpServer just reads bytes from InputStream

---
+++ src/main/java/com/soprasteria/http/HttpServer.java
@@ -0,0 +1,17 @@
+package com.soprasteria.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class HttpServer {
+
+    public static void main(String[] args) throws IOException {
+        var serverSocket = new ServerSocket(8080);
+        var clientSocket = serverSocket.accept();
+
+        int c;
+        while((c = clientSocket.getInputStream().read()) != -1) {
+            System.out.println(c);
+        }
+    }
+}
