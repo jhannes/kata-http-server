c4d5f2d5ab051546084a6d734285b43009743020
Johannes Brodwall <johannes@brodwall.com>
2023-02-14 22:45:45 +0100

    🔴 HttpServer should respond with path for unknown path

--- src/test/java/com/soprasteria/http/HttpServerTest.java
+++ src/test/java/com/soprasteria/http/HttpServerTest.java
@@ -2,6 +2,7 @@ package com.soprasteria.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -13,8 +14,13 @@ class HttpServerTest {
     @Test
     void shouldReturn404ForUnknownPath() throws IOException {
         var server = new HttpServer(0);
-        var connection = (HttpURLConnection) new URL(server.getURL(), "/unknown-path").openConnection();
+        var path = "/unknown-path";
+        var connection = (HttpURLConnection) new URL(server.getURL(), path).openConnection();
         assertEquals(404, connection.getResponseCode());
+        var buffer = new ByteArrayOutputStream();
+        connection.getErrorStream().transferTo(buffer);
+        var responseBody = buffer.toString();
+        assertEquals("Unknown path " + path, responseBody);
     }
 
 }
